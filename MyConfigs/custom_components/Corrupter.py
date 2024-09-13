4507e7cb9ae89579315f03538ce9d7ad1f2520c7
Sirbatiano <robertodelprete88@gmail.com>
2024-09-13 17:48:24 +0000

    chore: Update ImageCorruption class and remove unused code

--- MyConfigs/custom_components/Corrupter.py
+++ MyConfigs/custom_components/Corrupter.py
@@ -15,6 +15,9 @@ import numpy as np
 from scipy.ndimage import convolve
 from skimage import util
 
+from .aux import OpticalSystem
+
+
 def radiometric_spike_noise(array: np.ndarray, severity=.1):
     """
     Randomly amplifies or reduces the values in each column of a 2D or 3D array.
@@ -126,75 +129,42 @@ def corrupt_image(image: np.ndarray, noise_type: str, severity: float) -> np.nda
     return noisy_image.astype(np.float32)
 
 
-def add_snr_psf(
-    input_data: np.ndarray,
-    snr_values: dict,
-    psf_kernels: dict,
-    l_ref: float,
-    kernel_bands: list,
-    snr_bands: list
-) -> dict:
-    """
-    Adds SNR and PSF effects to input data using provided SNR values and PSF kernels.
-    
-    :param input_data: Input 4D array (t, h, w, d) where 'd' corresponds to different bands.
-    :param snr_values: A dictionary of SNR values for each band.
-    :param psf_kernels: A dictionary of PSF kernels for each band.
-    :param l_ref: Reference spectral radiance.
-    :param kernel_bands: List of band names corresponding to PSF kernels.
-    :param snr_bands: List of band names corresponding to SNR values.
-    :return: A dictionary with keys 'snr_added' and 'psf_added', containing the resulting data after SNR and PSF effects.
-    """
-
-    # Adding SNR noise to input data
-    random_noise = np.random.normal(size=input_data.shape)
-    
-    snr = np.array([snr_values[band] for band in snr_bands])
-    snr = np.reshape(snr, (1, 1, 1, len(snr)))  # Reshape to match input dimensions (t, h, w, d)
-    
-    noisy_data = input_data + l_ref * random_noise / snr  # Adding noise based on SNR
-    
-    # Applying PSF kernel convolution to the noisy data
-    psf_convolved_data = np.concatenate([
-        np.concatenate([
-            convolve(noisy_data[..., band], psf_kernels[kernel_band], mode="mirror")[..., np.newaxis]
-            for band, kernel_band in enumerate(kernel_bands)
-        ], axis=-1)[np.newaxis, ...]
-        for _data in noisy_data
-    ], axis=0)
-
-    return {
-        'snr_added': noisy_data,
-        'psf_added': psf_convolved_data
-    }
-
-
 
 @TRANSFORMS.register_module()
 class ImageCorruption(BaseTransform):
-    """Corruption augmentation.
-
-    Corruption transforms implemented based on
-    `skimage corruptions`.
-
-    Required Keys:
-
-    - img (np.uint8)
-
-
-    Modified Keys:
-
-    - img (np.uint8)
-
-
-    Args:
-        corruption (str): Corruption name.
-        severity (int): The severity of corruption. Defaults to 1.
+    """Corruption augmentation for images.
+    This class implements corruption transforms based on `skimage corruptions`.
+    Attributes:
+        sensor (str): The type of sensor, either 'sentinel' or 'venus'.
+        SNR (float): Signal-to-noise ratio, must be a positive number.
+        mtf_at_fe (float): Modulation transfer function at the Nyquist frequency.
+        - img (np.uint8): The image to be corrupted.
+        - img (np.uint8): The corrupted image.
+        sensor (str): The type of sensor, either 'sentinel' or 'venus'.
+        SNR (float, optional): Signal-to-noise ratio, default is 10.
+        mtf_at_fe (float, optional): Modulation transfer function at the Nyquist frequency, default is 0.2.
+    Raises:
+        AssertionError: If SNR is not a positive number.
+        AssertionError: If sensor is not 'sentinel' or 'venus'.
+        RuntimeError: If `imagecorruptions` is not installed.
+    Methods:
+        transform(results: dict) -> dict:
+            Applies corruption to the image in the results dictionary.
+        __repr__() -> str:
+            Returns a string representation of the ImageCorruption object.
     """
 
-    def __init__(self, corruption: str, severity: int = 1) -> None:
-        self.corruption = corruption
-        self.severity = severity
+    def __init__(self, sensor: str, SNR: float = 10, mtf_at_fe: float = 0.2) -> None:
+        self.sensor = sensor
+        self.SNR = SNR
+
+        self.mtf_at_fe = mtf_at_fe
+        assert SNR >= 0, 'SNR must be a positive number'
+        assert sensor in ['sentinel', 'venus'], 'sensor must be either sentinel or venus'
+        
+        self.optical_system = OpticalSystem(sensor, mtf_at_fe, SNR)
+
+
 
     def transform(self, results: dict) -> dict:
         """Call function to corrupt image.
@@ -208,10 +178,7 @@ class ImageCorruption(BaseTransform):
 
         if self.corruption is None:
             raise RuntimeError('imagecorruptions is not installed')
-        results['img'] = corrupt_image(
-            results['img'],
-            noise_type=self.corruption,
-            severity=self.severity)
+        results['img'] = self.optical_system.apply(results['img'])
         return results
 
     def __repr__(self) -> str:
@@ -219,3 +186,55 @@ class ImageCorruption(BaseTransform):
         repr_str += f'(corruption={self.corruption}, '
         repr_str += f'severity={self.severity})'
         return repr_str
+
+
+
+# @TRANSFORMS.register_module()
+# class ImageCorruption(BaseTransform):
+#     """Corruption augmentation.
+
+#     Corruption transforms implemented based on
+#     `skimage corruptions`.
+
+#     Required Keys:
+
+#     - img (np.uint8)
+
+
+#     Modified Keys:
+
+#     - img (np.uint8)
+
+
+#     Args:
+#         corruption (str): Corruption name.
+#         severity (int): The severity of corruption. Defaults to 1.
+#     """
+
+#     def __init__(self, corruption: str, severity: int = 1) -> None:
+#         self.corruption = corruption
+#         self.severity = severity
+
+#     def transform(self, results: dict) -> dict:
+#         """Call function to corrupt image.
+
+#         Args:
+#             results (dict): Result dict from loading pipeline.
+
+#         Returns:
+#             dict: Result dict with images corrupted.
+#         """
+
+#         if self.corruption is None:
+#             raise RuntimeError('imagecorruptions is not installed')
+#         results['img'] = corrupt_image(
+#                                 results['img'],
+#                                 noise_type=self.corruption,
+#                                 severity=self.severity)
+#         return results
+
+#     def __repr__(self) -> str:
+#         repr_str = self.__class__.__name__
+#         repr_str += f'(corruption={self.corruption}, '
+#         repr_str += f'severity={self.severity})'
+#         return repr_str
